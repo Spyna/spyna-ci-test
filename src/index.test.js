4d69b2c656175cbaca4d82922808776040c98b70
Spyna <lscaper@gmail.com>
2020-04-12 10:53:39 +0200

    fix: test

--- src/index.test.js
+++ src/index.test.js
@@ -3,11 +3,16 @@ describe("My lib tests", () => {
   beforeEach(() => {});
 
   afterEach(() => {});
-  let container = null;
 
   it("should greet a user", () => {
     const greet = Greet("spyna");
 
     expect(greet).toBe("Hello spyna");
+  });
+
+  it("should greet an undefined user", () => {
+    const greet = Greet();
+
+    expect(greet).toBe("Hello undefined");
   });
 });
